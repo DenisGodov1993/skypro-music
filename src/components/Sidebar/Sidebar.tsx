131382757885f2b4a1a09c56029f2c8911f1f98d
DenisGodov1993 <denisgodovanyuk@gmail.com>
2025-11-28 16:38:48 +0300

    I cleaned up the code and removed unnecessary comments.

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar() {
       <div className={styles.sidebar__block}>
         <div className={styles.sidebar__list}>
           <div className={styles.sidebar__item}>
-            <Link className={styles.sidebar__link} href="#"> 
+            <Link className={styles.sidebar__link} href="#">
               <Image
                 className={styles.sidebar__img}
                 src="/img/playlist01.png"
@@ -59,60 +59,3 @@ export default function Sidebar() {
     </div>
   );
 }
-
-
-// import styles from './sidebar.module.css';
-// import Link from 'next/link';
-// import Image from 'next/image';
-
-// export default function Sidebar() {
-//   return (
-//     <div className={styles.main__sidebar}>
-//       <div className={styles.sidebar__personal}>
-//         <p className={styles.sidebar__personalName}>Sergey.Ivanov</p>
-//         <div className={styles.sidebar__icon}>
-//           <svg>
-//             <use xlinkHref="/img/icon/sprite.svg#logout"></use>
-//           </svg>
-//         </div>
-//       </div>
-//       <div className={styles.sidebar__block}>
-//         <div className={styles.sidebar__list}>
-//           <div className={styles.sidebar__item}>
-//             <Link className={styles.sidebar__link} href="#"> 
-//               <Image
-//                 className={styles.sidebar__img}
-//                 src="/img/playlist01.png"
-//                 alt="day's playlist"
-//                 width={250}
-//                 height={170}
-//               />
-//             </Link>
-//           </div>
-//           <div className={styles.sidebar__item}>
-//             <Link className={styles.sidebar__link} href="#">
-//               <Image
-//                 className={styles.sidebar__img}
-//                 src="/img/playlist02.png"
-//                 alt="day's playlist"
-//                 width={250}
-//                 height={170}
-//               />
-//             </Link>
-//           </div>
-//           <div className={styles.sidebar__item}>
-//             <Link className={styles.sidebar__link} href="#">
-//               <Image
-//                 className={styles.sidebar__img}
-//                 src="/img/playlist03.png"
-//                 alt="day's playlist"
-//                 width={250}
-//                 height={170}
-//               />
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
--- src/store/features/trackSlice.ts
+++ src/store/features/trackSlice.ts
@@ -36,16 +36,6 @@ const trackSlice = createSlice({
     toggleShuffle: (state) => {
       state.isShuffle = !state.isShuffle;
     },
-    // setNextTrack: (state) => {
-    //   const playlist = state.isShuffle
-    //     ? state.shuffledPlaylist
-    //     : state.playlist;
-    //   const curIndex = state.playlist.findIndex(
-    //     (el) => el._id === state.currentTrack?._id,
-    //   );
-    //   const nextIndexTrack = curIndex + 1;
-    //   state.currentTrack = playlist[nextIndexTrack];
-    // },
 
     setNextTrack: (state) => {
       const list = state.isShuffle ? state.shuffledPlaylist : state.playlist;
@@ -59,21 +49,12 @@ const trackSlice = createSlice({
       }
     },
 
-    // setNextTrack: (state) => {
-    //   const list = state.isShuffle ? state.shuffledPlaylist : state.playlist;
-
-    //   const index = list.findIndex(el => el._id === state.currentTrack?._id);
-    //   const next = index + 1;
-
-    //   if (next < list.length) {
-    //     state.currentTrack = list[next];
-    //   }
-    // },
-
     setPrevTrack: (state) => {
       const list = state.isShuffle ? state.shuffledPlaylist : state.playlist;
 
-      const curIndex = list.findIndex((el) => el._id === state.currentTrack?._id);
+      const curIndex = list.findIndex(
+        (el) => el._id === state.currentTrack?._id,
+      );
       const prev = curIndex - 1;
 
       if (prev >= 0) {
@@ -92,170 +73,3 @@ export const {
   toggleShuffle,
 } = trackSlice.actions;
 export const trackSliceReducer = trackSlice.reducer;
-
-// до финальной версии
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import { TrackType } from '@sharedTypes/sharedTypes';
-
-// type initialStateType = {
-//   currentTrack: TrackType | null;
-//   isPlay: boolean;
-//   playlist: TrackType[];
-//   shuffledPlaylist: TrackType[];
-//   isShuffle: boolean;
-// };
-
-// const initialState: initialStateType = {
-//   currentTrack: null,
-//   isPlay: false,
-//   playlist: [],
-//   shuffledPlaylist: [],
-//   isShuffle: false,
-// };
-
-// const trackSlice = createSlice({
-//   name: 'tacks',
-//   initialState,
-//   reducers: {
-//     setCurrentTrack: (state, action: PayloadAction<TrackType>) => {
-//       state.currentTrack = action.payload;
-//     },
-//     setCurrentPlaylist: (state, action: PayloadAction<TrackType[]>) => {
-//       state.playlist = action.payload;
-//       state.shuffledPlaylist = [...state.playlist].sort(
-//         () => Math.random() - 0.5,
-//       );
-//     },
-//     setIsPlay: (state, action: PayloadAction<boolean>) => {
-//       state.isPlay = action.payload;
-//     },
-//     toggleShuffle: (state) => {
-//       state.isShuffle = !state.isShuffle;
-//     },
-//     setNextTrack: (state) => {
-//       const playlist = state.isShuffle
-//         ? state.shuffledPlaylist
-//         : state.playlist;
-//       const curIndex = state.playlist.findIndex(
-//         (el) => el._id === state.currentTrack?._id,
-//       );
-//       const nextIndexTrack = curIndex + 1;
-//       state.currentTrack = playlist[nextIndexTrack];
-//     },
-//   },
-// });
-
-// export const {
-//   setCurrentTrack,
-//   setIsPlay,
-//   setCurrentPlaylist,
-//   setNextTrack,
-//   toggleShuffle,
-// } = trackSlice.actions;
-// export const trackSliceReducer = trackSlice.reducer;
-
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import { TrackType } from '@sharedTypes/sharedTypes';
-
-// type initialStateType = {
-//   currentTrack: TrackType | null;
-//   isPlay: boolean;
-//   playlist: TrackType[];
-// };
-
-// const initialState: initialStateType = {
-//   currentTrack: null,
-//   isPlay: false,
-//   playlist: [],
-// };
-
-// const trackSlice = createSlice({
-//   name: 'tacks',
-//   initialState,
-//   reducers: {
-//     setCurrentTrack: (state, action: PayloadAction<TrackType>) => {
-//       state.currentTrack = action.payload;
-//       // state.isPlay = true;
-//     },
-//     setCurrentPlaylist: (state, action: PayloadAction<TrackType[]>) => {
-//       state.playlist = action.payload;
-//     },
-//     setIsPlay: (state, action: PayloadAction<boolean>) => {
-//       state.isPlay = action.payload;
-//     },
-//     setNextTrack: (state) => {
-//       if (state.currentTrack) {
-//         const curIndex = state.playlist.findIndex(
-//           (el) => el._id === state.currentTrack?._id,
-//         );
-//         const nextIndexTrack = curIndex + 1;
-//         state.currentTrack = state.playlist[nextIndexTrack];
-//       }
-//     },
-//   },
-// });
-
-// export const { setCurrentTrack, setIsPlay, setCurrentPlaylist, setNextTrack } =
-//   trackSlice.actions;
-// export const trackSliceReducer = trackSlice.reducer;
-
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-// import { TrackType } from '@sharedTypes/sharedTypes';
-
-// type initialStateType = {
-//   currentTrack: TrackType | null;
-//   isPlay: boolean;
-// };
-
-// const initialState: initialStateType = {
-//   currentTrack: null,
-//   isPlay: false,
-// };
-
-// const trackSlice = createSlice({
-//   name: 'tacks',
-//   initialState,
-//   reducers: {
-//     setCurrentTrack: (state, action: PayloadAction<TrackType>) => {
-//       state.currentTrack = action.payload;
-//       state.isPlay = true;
-//     },
-//     setIsPlay: (state, action: PayloadAction<boolean>) => {
-//       state.isPlay = action.payload;
-//     },
-//   },
-// });
-
-// export const { setCurrentTrack, setIsPlay } = trackSlice.actions;
-// // export const { setCurrentTrack, setIsPlaying } = trackSlice.actions;
-// export const trackSliceReducer = trackSlice.reducer;
-
-// import { TrackType } from '@sharedTypes/sharedTypes';
-// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-// type initialStateType = {
-//   currentTrack: null | TrackType;
-//   isPlay: boolean;
-// };
-
-// const initialState: initialStateType = {
-//   currentTrack: null,
-//   isPlay: false,
-// };
-
-// const trackSlice = createSlice({
-//   name: 'tacks',
-//   initialState,
-//   reducers: {
-//     setCurrentTrack: (state, action: PayloadAction<TrackType>) => {
-//       state.currentTrack = action.payload;
-//     },
-//     setIsPlay: (state, action: PayloadAction<boolean>) => {
-//       state.isPlay = action.payload;
-//     },
-//   },
-// });
-
-// // export const { setCurrentTrack } = trackSlice.actions;
-// export const { setCurrentTrack, setIsPlay } = trackSlice.actions;
-// export const trackSliceReducer = trackSlice.reducer;
