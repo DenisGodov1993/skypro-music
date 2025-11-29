bd9d068a433bbce7895e8d62628f0643f81342fa
DenisGodov1993 <denisgodovanyuk@gmail.com>
2025-11-29 16:21:51 +0300

    structured project

---
+++ src/app/music/category/[id]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
+export default function CategoryPage() {
+  const params = useParams<{ id: string }>();
+  return <h1>Категория {params.id}</h1>;
+}
+//  создать лаяулт для категорий, вместо реки, должны быть названия плейлистов
